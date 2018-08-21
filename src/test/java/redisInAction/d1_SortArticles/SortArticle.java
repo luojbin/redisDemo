d5295d6152cc7cca709809060a8b8aa52b4c06dd
luojbin <z1_2x@msn.com>
2018-08-21 18:02:39 +0800

    分组功能

--- src/test/java/redisInAction/d1_SortArticles/SortArticle.java
+++ src/test/java/redisInAction/d1_SortArticles/SortArticle.java
@@ -3,6 +3,7 @@ package redisInAction.d1_SortArticles;
 import org.junit.Before;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,7 +42,7 @@ public class SortArticle {
      */
     private final String ARTICLE = "article:";
     private final String VOTED_USER = "voted:";
-    private final String SCORE = "200";
+    private final Long SCORE_PER_VOTE = 432L;
 
     @Test
     public void testInit() {
@@ -58,11 +59,12 @@ public class SortArticle {
     public void testPublish() {
 
         String articleId = "" + jedis.incr("articleId");
-        String author = "luojbin";
-        String body = "这是一篇文章, 关于: " + articleId;
+        String title = "color:yellow";
+        String author = "luojbin33";
+        String body = "这是一篇文章, 关于: " + title;
 
         Map<String, String> article = new HashMap<>();
-        article.put("title", articleId);
+        article.put("title", title);
         article.put("author", author);
         article.put("body", body);
         article.put("votes", "0");
@@ -73,38 +75,59 @@ public class SortArticle {
         // 创建文章的 投票用户set
         jedis.sadd(VOTED_USER + articleId, "");
 
+        long now = System.currentTimeMillis() / 1000;
         // 把文章添加到 发布时间 zset
-        jedis.zadd(PUBLISH_TIME_ZSET, System.currentTimeMillis() / 1000, ARTICLE + articleId);
+        jedis.zadd(PUBLISH_TIME_ZSET, now, ARTICLE + articleId);
 
         // 把文章添加到 文章分数 zset
-        jedis.zadd(ARTICLE_SCORE_ZSET, System.currentTimeMillis() / 1000, ARTICLE + articleId);
+        jedis.zadd(ARTICLE_SCORE_ZSET, now, ARTICLE + articleId);
+
+        System.out.println("发布成功!");
     }
 
     @Test
-    public void testGetArticles() {
+    public void testGetArticlesByCTimeDesc() {
         // 在指定的 zset 中, 获取排名在 [start, end] 范围内的成员 key (文章id)
-        Set<String> ids = jedis.zrevrange(PUBLISH_TIME_ZSET, 0, -1);
+        Set<Tuple> ids = jedis.zrevrangeWithScores(PUBLISH_TIME_ZSET, 0, -1);
         // 根据文章ids, 遍历查出文章 hash
-        for (String id : ids) {
-            Map<String, String> articleData = jedis.hgetAll(id);
-            articleData.put("id", id);
-            System.out.println("这是一篇文章: " + id);
+        printArticles(ids);
+    }
+
+    @Test
+    public void testGetArticlesByScoreDesc() {
+        // 在指定的 zset 中, 获取排名在 [start, end] 范围内的成员 key (文章id)
+        Set<Tuple> ids = jedis.zrevrangeWithScores(ARTICLE_SCORE_ZSET, 0, -1);
+        // 根据文章ids, 遍历查出文章 hash
+        printArticles(ids);
+    }
+
+    public void printArticles(Set<Tuple> ids){
+        for (Tuple id : ids) {
+            Map<String, String> articleData = jedis.hgetAll(id.getElement());
+            System.out.println("这是一篇文章: " + id.getElement() + ", 分数为:" + (long)id.getScore());
             for (Map.Entry<String, String> entry : articleData.entrySet()) {
-                if (entry.getKey().equals("id")) {
-                    continue;
-                }
-                System.out.println("    " + entry.getKey() + ": " + entry.getValue());
+                System.out.println("\t" + entry.getKey() + ": " + entry.getValue());
             }
         }
     }
-
     /**
      * 为文章投票-赞
      */
     @Test
     public void testVoteUp() {
-        String articleId = "10003";
-
+        String articleId = "10010";
+        String userId = "luojbin7";
+        // 需要在文章投票用户表中记录
+        Long voted = jedis.sadd(VOTED_USER+articleId, userId);
+        if(!new Long(1).equals(voted)){
+            System.out.println("该用户已为当前文章投票, 不可重复投票!");
+            return;
+        }
+        // 更新投票人数
+        jedis.hincrBy(ARTICLE+articleId, "votes", 1);
+        // 为文章加分
+        long score = (long)(double)jedis.zincrby(ARTICLE_SCORE_ZSET, SCORE_PER_VOTE, ARTICLE+articleId );
+        System.out.println("投票后, " + ARTICLE + articleId + "的分数为: " + score);
     }
 
     /**
@@ -119,19 +142,55 @@ public class SortArticle {
      * 获取文章列表
      */
     @Test
-    public void testGetListByScore() {
-        Set<String> articleSet = jedis.zrevrange(ARTICLE_SCORE_ZSET, 0, -1);
-        for (String article : articleSet) {
-            System.out.print(article + ": ");
-            System.out.println(jedis.zscore(ARTICLE_SCORE_ZSET, article));
+    public void testGetScores() {
+        Set<Tuple> scoreSet = jedis.zrevrangeWithScores(ARTICLE_SCORE_ZSET, 0, -1);
+        for(Tuple tuple : scoreSet){
+            System.out.println(tuple.getElement() + " 的分数为: " + (long)tuple.getScore());
         }
     }
 
     /**
-     * 分组查询
+     * 将文章添加到分组
      */
     @Test
     public void testGroup() {
+        // // animals 分组
+        // jedis.sadd("group:animals", ARTICLE+"10004");
+        // jedis.sadd("group:animals", ARTICLE+"10005");
+        // jedis.sadd("group:animals", ARTICLE+"10006");
+        // jedis.sadd("group:animals", ARTICLE+"10007");
+        //
+        // // fruit 分组
+        // jedis.sadd("group:fruit", ARTICLE+"10008");
+        // jedis.sadd("group:fruit", ARTICLE+"10009");
+        // jedis.sadd("group:fruit", ARTICLE+"10010");
+        // jedis.sadd("group:fruit", ARTICLE+"10011");
+        //
+        // // color 分组
+        // jedis.sadd("group:color", ARTICLE+"10012");
+        // jedis.sadd("group:color", ARTICLE+"10013");
+        // jedis.sadd("group:color", ARTICLE+"10014");
+        // jedis.sadd("group:color", ARTICLE+"10015");
 
+        // fruit 分组
+
+
+    }
+
+    /**
+     * 将文章移出分组
+     */
+    @Test
+    public void testRemoveFromGroup() {
+
+    }
+    /**
+     * 按分组获取文章
+     */
+    @Test
+    public void testArticlesByGroup() {
+        jedis.zinterstore("zgroup:fruit",ARTICLE_SCORE_ZSET, "group:fruit");
+        Set<Tuple> set = jedis.zrevrangeWithScores("zgroup:fruit", 0, -1);
+        printArticles(set);
     }
 }
